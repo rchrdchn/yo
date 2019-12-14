a441ec6dd825e1feddade3ea74015a3923366cfa
Richard Chan <richardchanme@gmail.com>
2019-12-13 16:02:23 -0800

    Updates

---
+++ precache-manifest.2d88e0e813bc5167a2831fbe52cb19fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41955a809f4fd4c2e5b074474beeee2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50178b88d09136ebe4c0",
+    "url": "/static/js/2.f4584181.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f4584181.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7469109f4872ff389591",
+    "url": "/static/js/main.38fe6534.chunk.js"
+  },
+  {
+    "revision": "65929b50180444b1792b",
+    "url": "/static/js/runtime-main.c667adde.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7763c2b17e63eddb29e7557a0050f426.js"
+  "/precache-manifest.2d88e0e813bc5167a2831fbe52cb19fd.js"
 );
 
 self.addEventListener('message', (event) => {
