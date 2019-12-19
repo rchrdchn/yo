bb70378d4f6e9f66c44c67eb27010ce0f1f7115c
Richard Chan <richardchanme@gmail.com>
2019-12-19 13:15:06 -0800

    Working on Readings page

---
+++ src/components/books.js
@@ -0,0 +1,125 @@
+import Sapiens from '../assets/covers/sapiens.jpg';
+
+const Books = [
+    {
+        "title": "Homo Deus",
+        "author": "Yuval Noah Harari",
+        "cover": "A",
+        "link": "https://amzn.to/2PBamaQ",
+        "reading": true
+    },
+    {
+        "title": "Upheaval",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/2SdahMd",
+        "reading": true
+    },
+    {
+        "title": "Cien Años de Soledad",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/2S56D6U",
+        "reading": false
+    },
+    {
+        "title": "Medidations",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/35NAqEY",
+        "reading": false
+    },
+    {
+        "title": "From Ramen to Riches",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/2S6LVne",
+        "reading": false
+    },
+    {
+        "title": "My Turn: A Life of Total Football",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/2EyGaqo",
+        "reading": true
+    },
+    {
+        "title": "The Courage to be Disliked",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/38VGHAv",
+        "reading": false
+    },
+    {
+        "title": "Anything You Want",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/35BaiNR",
+        "reading": false
+    },
+    {
+        "title": "Sapiens",
+        "author": "Yuval Noah Harari",
+        "cover": Sapiens,
+        "link": "https://amzn.to/2rS77CO",
+        "reading": false
+    },
+    {
+        "title": "Shape Up",
+        "author": "B",
+        "cover": "B",
+        "link": "https://basecamp.com/shapeup",
+        "reading": false
+    },
+    {
+        "title": "Daily Stoics",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/35AiCgx",
+        "reading": false
+    },
+    {
+        "title": "Zen Mind, Beginner's Mind",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/38RNtHF",
+        "reading": false
+    },
+    {
+        "title": "Rich People Problems",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/34DNsng",
+        "reading": false
+    },
+    {
+        "title": "Shoe Dog: A Memoir by the Creator of Nike",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/2S63VxZ",
+        "reading": false
+    },
+    {
+        "title": "The Manual: A Philosopher's Guide to Life",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/34CNqfg",
+        "reading": false
+    },
+    {
+        "title": "Essentialism: The Disciplined Pursuit of Less",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/38UjpLm",
+        "reading": false
+    },
+    {
+        "title": "Ikigai: The Japanese Secret to a Long and Happy Life",
+        "author": "B",
+        "cover": "B",
+        "link": "https://amzn.to/35BM9GH",
+        "reading": false
+    }
+]
+
+export default Books;
---
+++ src/components/readings.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import books from './books.js';
+import '../App.scss';
+
+const Readings = () => {
+    const renderHTML = book => (
+        <div className="reads_book" key={book.title}>
+            <div className="reads_cover">
+                <img src={book.cover} alt={book.title}/>
+            </div>
+            <div className="reads_metadata">
+                <h3><a href={book.link} target="_blank" rel="noopener noreferrer">{book.title}</a></h3>
+                <p>{book.author}</p>
+            </div>
+        </div>
+    )
+
+    const current = books => books.filter(book => book.reading !== false).map(book => renderHTML(book));
+
+    const past = books => books.filter(book => book.reading !== true).map(book => renderHTML(book));
+
+    return (
+        <div className="reads">
+            <h2>Reading list <span role="img" aria-label="emoji">📚</span></h2>
+            <div className="reads_current">
+                <h3>Current</h3>
+                {current(books)}
+            </div>
+            <div className="reads_past">
+                <h3>Past</h3>
+                {past(books)}
+            </div>
+        </div>
+    )
+}
+
+export default Readings;
