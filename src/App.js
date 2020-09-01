8b5d7a38f707652ab8e84eb0383f17600543406a
Richard Chan <richardchanme@gmail.com>
2020-08-31 18:34:52 -0700

    Removed dark-mode work, reverted colors to bg black and font-color white

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -11,41 +11,10 @@ import Now from './components/now';
 import Readings from './components/readings';
 
 const App = () => {
-	const getPrefColorScheme = () => {
-		if (!window.matchMedia) return;
-	
-		return window.matchMedia("(prefers-color-scheme: dark)").matches;
-	}
-
-	const getInitialMode = () => {
-		const isReturningUser = "dark" in localStorage;
-		const savedMode = JSON.parse(localStorage.getItem("dark"));
-		const userPrefersDark = getPrefColorScheme();
-		// if mode was saved --> dark / light
-		if (isReturningUser) {
-		return savedMode;
-		// if preferred color scheme is dark --> dark
-		} else if (userPrefersDark) {
-		return true;
-		// otherwise --> light
-		} else {
-		return false;
-		}
-		// return savedMode || false;
-	}
-
-	const [darkMode, setDarkMode] = useState(getInitialMode());
-
-	useEffect(() => {
-		localStorage.setItem("dark", JSON.stringify(darkMode));
-	}, [darkMode])
-
-
-
 	return (
 		<Router>
-			<div className={`container ${darkMode ? "dark-mode" : "light-mode"}`}>
-				<Navbar onChangeMode={() => setDarkMode(prevMode => !prevMode)} darkMode={darkMode}/>
+			<div>
+				<Navbar />
 				<Switch>
 					<Route exact path="/">
 						<Home />
