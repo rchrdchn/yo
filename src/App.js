40d615a115d3345cce4c9af16c1c08452263bc1a
Richard Chan <richardchanme@gmail.com>
2021-01-05 21:28:00 -0800

    Added new components and routes for Things I Enjoy and Gear

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Navbar from './components/navbar';
 import Home from './components/home';
 import Now from './components/now';
 import Readings from './components/readings';
+import Gear from './components/gear';
+import Enjoy from './components/enjoy';
 
 const App = () => {
 	return (
@@ -24,6 +26,12 @@ const App = () => {
 					</Route>
 					<Route exact path="/readings">
 						<Readings />
+					</Route>
+					<Route exact path="/gear">
+						<Gear />
+					</Route>
+					<Route exact path="/enjoy">
+						<Enjoy />
 					</Route>
 				</Switch>
 			</div>
